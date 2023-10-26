ae0fd5995efb55285eff14311023bf3ddeca9be7
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2023-10-25 22:22:35 -0300

    Maze structure

---
+++ src/algorithms/maze.ts
@@ -0,0 +1,87 @@
+import { Neighbor, Node, Searchable } from "../commons/searchable";
+
+export class MazeNode implements Node {
+    constructor(
+        public readonly value: number,
+        public readonly row: number,
+        public readonly column: number,
+    ) { }
+}
+
+export const MAZE1 = `
+#@#######
+# #     #
+# ### ###
+#   # # #
+### # # #
+#   #   #
+### # # #
+#     # #
+#####X###`;
+
+const charMap = {
+    ' ': 0,
+    '#': 1,
+    '@': 2,
+    'X': 3,
+};
+
+export class Maze implements Searchable {
+    private readonly matrix: number[][];
+    private width: number;
+    private height: number;
+    public initialNode: MazeNode;
+
+    constructor(data: string) {
+        this.matrix = data
+            .trim()
+            .split('\n')
+            .map(l => l.split('')
+                       .map(v => charMap[v] ?? -1));
+
+        this.height = this.matrix.length;
+        this.width = this.matrix[0]?.length ?? 0;
+
+        this.forEachCell((row: number, col: number, value: number) => {
+            if(value == 2) {
+                this.initialNode = this.getNode(row, col);
+            }
+        });
+    }
+
+    getNeighbors(src: MazeNode): Neighbor[] {
+        return [
+            this.getNode(src.row - 1, src.column),
+            this.getNode(src.row + 1, src.column),
+            this.getNode(src.row, src.column - 1),
+            this.getNode(src.row, src.column + 1),
+        ].filter(Boolean)
+         .map(n => Neighbor.of(n, 1));
+    }
+
+    getGlobalCost(src: MazeNode, dst: MazeNode): number {
+        const deltaXSq = (src.column - dst.column)**2;
+        const deltaYSq = (src.row - dst.row)**2;
+        return Math.sqrt(deltaXSq + deltaYSq);
+    }
+
+    private getNode(row: number, col: number): MazeNode | null {
+        if(row < 0 || col < 0) {
+            return null;
+        }
+
+        if(row >= this.height || col >= this.width) {
+            return null;
+        }
+
+        return new MazeNode(this.matrix[row][col], row, col);
+    }
+
+    private forEachCell(fn: (row: number, col: number, value: number) => void): void {
+        for(const [rowNum, cols] of this.matrix.entries()) {
+            for(const [colNum, value] of cols.entries()) {
+                fn(rowNum, colNum, value);
+            }
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-console.log('main.js');
+import { MAZE1, Maze } from "./algorithms/maze";
+
+const maze = new Maze(MAZE1);
+console.log(maze.getNeighbors(maze.initialNode));
+
+console.log(maze.getGlobalCost(maze.initialNode, maze['getNode'](0, 0)));
