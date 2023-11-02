ad41b6e0048b110aa7732a72b860886a375905ea
Ricardo Zamboni <ricardozamboni021@gmail.com>
2023-11-02 13:50:43 -0300

    functional dfs algorithm

--- src/algorithms/maze/idfs.ts
+++ src/algorithms/maze/idfs.ts
@@ -3,28 +3,30 @@ import { Maze, MazeNode } from "./maze";
 
 export class IterativeDepthFirstSearch {
   public maze: Maze;
-  public currentNode: MazeNode;
+  public initialNode: MazeNode;
   public stack: MazeNode[];
   public visitedNodes: MazeNode[];
   public readonly limit;
 
   constructor(maze: Maze, limit: number){
     this.maze = maze;
-    this.currentNode = maze.initialNode;
-    this.stack = [maze.initialNode];
-    this.visitedNodes = [maze.initialNode];
+    this.initialNode = maze.initialNode;
+    this.stack = [];
+    this.visitedNodes = [];
     this.limit = limit;
   }
 
   showCurrentStack() {
-    console.log(`stack -> ${this.stack.map((node) => `${node.column},${node.row}`).join(' ')}`)
+    console.log(`stack -> ${this.stack.map((node) => ` (${node.column},${node.row})`).join(' ')}`);
   }
 
   showVisitedNodes() {
-    console.log(`nos visitado -> ${this.visitedNodes.map(neighbor => `${neighbor['column']},${neighbor['row']}`).join(' ')}`);
+    console.log(`nos visitado -> ${this.visitedNodes.map(neighbor => `(${neighbor['column']},${neighbor['row']})`).join(' ')}`);
   }
 
   addNodeToStack(node: MazeNode) {
+    if(this.stack.find(stackNode => stackNode.column === node.column && stackNode.row === node.row)) return;
+
     this.stack.push(node);
   }
 
@@ -34,15 +36,11 @@ export class IterativeDepthFirstSearch {
     this.visitedNodes.push(node);
   }
 
-  updateCurrentNode(node: MazeNode) {
-    this.currentNode = node;
-  }
-
   validateNeighbors(neighbors: Neighbor[]) {
     const nodes = neighbors.map(neighbor => neighbor.node);
   
     const filteredNeighbors = nodes.filter((neighbor) => {
-      return !this.stack.some((node) => {
+      return !this.visitedNodes.some((node) => {
         return (
           node.value === neighbor['value'] &&
           node.row === neighbor['row'] &&
@@ -54,63 +52,46 @@ export class IterativeDepthFirstSearch {
     return filteredNeighbors;
   }
 
-  exploreNode(currentNode: MazeNode) {
-    if(currentNode.value === 3){ return; }
+  exploreNode(currentNode: MazeNode): Boolean {
+    this.addNodeToVisited(currentNode);
+    
+    // Caso base: Verificar se a posição atual é a saída
+    if(currentNode.value === 3){
+      return true; 
+    }
 
-    console.log();
-    console.log(`estou -> ${currentNode['column']},${currentNode['row']}`);
-    this.showCurrentStack();
-    this.showVisitedNodes();
-
+    // Caso recursivo: Posição atual ainda não foi visitada 
     const neighbors = this.maze
         .getNeighbors(currentNode)
-        .filter(neighbor => neighbor.node['value'] !== 1)
+        .filter(neighbor => neighbor.node['value'] !== 1);
         
     const validNeighbors = this.validateNeighbors(neighbors as Neighbor[]);
 
-    if(validNeighbors.length > 0) {
-      console.log(`vizinhos gerais de ${currentNode.column},${currentNode.row} -> ${neighbors.map(neighbor => `${neighbor.node['column']},${neighbor.node['row']}`).join(' ')}`);
-      console.log(`vizinhos possíveis de ${currentNode.column},${currentNode.row} -> ${validNeighbors.map(neighbor => `${neighbor['column']},${neighbor['row']}`).join(' ')}`);
-    } else {
-      console.log(`o no ${currentNode['column']},${currentNode['row']} nao possui vizinhos`);
-    }
+    for(var i = 0; i < validNeighbors.length; i++) {
+      const node = new MazeNode(
+        validNeighbors[i]['value'], 
+        validNeighbors[i]['row'], 
+        validNeighbors[i]['column']
+      );
 
+      if(this.exploreNode(node)){
+        this.addNodeToStack(node);
+
+        return true;
+      };
+    }
     
-    if(validNeighbors.length !== 0) {
-      this.updateCurrentNode(validNeighbors[0] as MazeNode)
-
-      console.log(`vou para ${validNeighbors[0]['column']},${validNeighbors[0]['row']}`);
-      this.addNodeToVisited(validNeighbors[0] as MazeNode);
-      this.addNodeToStack(validNeighbors[0] as  MazeNode);
-
-      // this.exploreNode(this.currentNode);
-
-    } else {
-
-      this.stack.pop()
-      this.updateCurrentNode(this.stack[this.stack.length-1])
-      // this.exploreNode(this.currentNode);
-    }
+    return false;
   }
 
   start() {
-    this.exploreNode(this.currentNode);
-    console.log()
+    this.exploreNode(this.initialNode);
 
-    this.exploreNode(this.currentNode);
-    console.log()
+    this.addNodeToStack(this.initialNode);
+    this.stack.reverse();
 
-    this.exploreNode(this.currentNode);
-    console.log()
-
-    this.exploreNode(this.currentNode);
-    console.log()
-
-    this.exploreNode(this.currentNode);
-    console.log()
-
-    // console.log("Encontramos o melhor caminho");
-    // this.showCurrentStack()
-
+    console.log();
+    this.showCurrentStack();
+    this.showVisitedNodes();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { IterativeDepthFirstSearch } from "./algorithms/maze/idfs";
 import { Maze } from "./algorithms/maze/maze";
 import { MAZE1, MAZE2, MAZE3 } from "./algorithms/maze/utils";
 
-const maze = new Maze(MAZE2);
+const maze = new Maze(MAZE3);
 const idfs = new IterativeDepthFirstSearch(maze, 3);
 
 console.log(maze);
